1e6dbfcd62c62a30f85434e9b03d55153a279b95
ines563 <sakkaines20@gmail.com>
2021-04-27 00:58:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+/* eslint-disable jsx-a11y/alt-text */
+
 import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+return (
+<div className="App">
+    <div style={{border:"solid 1px black", maxWidth:"100vw", margin:"70px"}}>
+        <h1 className="title red">Your name here</h1>
+        <img src="/logo512.png" className="pub"/>
+        <img src={logo} className="src"/>
     </div>
-  );
+        <ReactPlayer width='320px' height='240px' controls 
+            url="https://youtu.be/UZCO5k1Nu70"
+      />
+        
+   
+
+</div>
+);
 }
 
 export default App;
